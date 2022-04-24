6cecb50ae535952d057832c50f4cd04b77f9640e
RoUchiha <swaggsikh@gmail.com>
2022-04-23 20:01:28 -0500

    commit tree 5f5e55da85977cf7e8192da98a33bb01cc096aac

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,52 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.state.username || !this.state.password) {
+      null
+        } else {
+          this.props.handleLogin(event)
+        }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(event) => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={(event) => this.handleChange(event)} 
+            id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={(event) => this.handleChange(event)}
+            id="password" name="password" type="password" />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button 
+          type="submit">Log in</button>
         </div>
       </form>
     );
